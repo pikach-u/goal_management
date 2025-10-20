bf10bfa3579b2891ef39140f9f9e494fef057cc7
pikach-u <ziwon05@naver.com>
2025-10-20 17:52:48 +0900

    JWT secret 생성을 위한 KeyGenerator 추가, User Entity 작성중

---
+++ backend/src/main/java/com/team9/backend/user/entity/User.java
@@ -0,0 +1,84 @@
+package com.team9.backend.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users") // 예약어 충돌 방지
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String userId;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String bio;
+    private String provider;      // LOCAL, GOOGLE, GITHUB
+    private String providerId;
+    private Boolean enabled = true;
+
+    private String role;          // USER, ADMIN
+    private String motivationType;  // default: ??
+    private boolean goalVisibility;  // 친구공개 추가: bool -> enum
+    private boolean progressVisibility;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+//    private List<Goal> goals = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+//    private List<Post> posts = new ArrayList<>();
+
+    // 기본 사용자 권한 부여
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role));  //ROLE_USER or ROLE_ADMIN
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    // Lombok(@Data)이 자동 생성하지 않는 UserDetails의 기본 메서드도
+    // 명시적으로 구현
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
