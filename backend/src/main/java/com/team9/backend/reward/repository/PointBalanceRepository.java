0511c2ceba1a9a3096420f65b8604aa3a1d24a27
pikach-u <ziwon05@naver.com>
2025-10-26 04:50:56 +0900

    오류 부분 임시로 삭제

--- backend/src/main/java/com/team9/backend/reward/repository/PointBalanceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team9.backend.reward.repository;
-
-import com.team9.backend.reward.entity.PointsBalance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PointBalanceRepository extends JpaRepository<PointsBalance, Long> {
-}
--- backend/src/main/java/com/team9/backend/reward/repository/PointTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team9.backend.reward.repository;
-
-import com.team9.backend.reward.entity.PointTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PointTransactionRepository extends JpaRepository<PointTransaction, Long> {
-    List<PointTransaction> findByUserIdOrderByCreatedAtDesc(Long userId);
-}
